d57158b5b380cce7a4ad34e8caac529f6cb0ef34
nmeka <nmeka@INBENNMEKA.atrapa.deloitte.com>
2020-02-13 15:31:01 +0530

    java-spring-poc3

---
+++ src/main/java/com/techapp/spring/soap/api/SoapSpringBootWs3Application.java
@@ -0,0 +1,13 @@
+package com.techapp.spring.soap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapSpringBootWs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapSpringBootWs3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/techapp/spring/soap/api/SoapSpringBootWs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techapp.spring.soap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapSpringBootWs3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
